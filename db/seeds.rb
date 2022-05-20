a289d52ef6ace5ae073c9915e89d8da50ca8019b
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-20 13:01:27 -0500

    Added application destroy all

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
   Pet.destroy_all
   VeterinaryOffice.destroy_all
   Veterinarian.destroy_all
+  Application.destroy_all
 
   @shelter_1 = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
   @shelter_2 = Shelter.create!(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
