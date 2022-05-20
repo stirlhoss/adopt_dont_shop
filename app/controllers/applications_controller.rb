dbf5161423212af2330c65b74a834c94b923bc1e
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-20 15:28:34 -0500

    Added: new create and app_params  methods

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,7 +2,22 @@ class ApplicationsController < ApplicationController
    def index
      @applications = Application.all
    end
+
    def show
      @application = Application.find(params[:id])
+   end
+
+   def new
+     # @application = Application.find(params[:id])
+   end
+
+   def create
+     application = Application.create(app_params)
+     redirect_to "/applications/#{application.id}"
+   end
+
+   private
+   def app_params
+     params.permit(:name,:address,:city,:state,:zipcode,:description, :status)
    end
  end
