2001a5417f31defd6b2af4201a7b217297cc563c
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-20 17:50:24 -0500

    Added Search parameters for show method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,6 +5,11 @@ class ApplicationsController < ApplicationController
 
    def show
      @application = Application.find(params[:id])
+     if params[:search].present?
+       @searched_pets = Pet.search(params[:search])
+     else
+       @searched_pets
+     end
    end
 
    def new
