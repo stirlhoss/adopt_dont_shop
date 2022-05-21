2a01d92cd04d8e73e798def2fdecd7eea192a3d6
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-21 13:29:17 -0500

    added update method

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -5,8 +5,10 @@ class ApplicationsController < ApplicationController
 
    def show
      @application = Application.find(params[:id])
+
      @application.pets << Pet.find(params[:pet]) if params[:pet]
      @applied_pets = @application.pets
+
      if params[:search].present?
        @searched_pets = Pet.search(params[:search])
      else
@@ -18,13 +20,21 @@ class ApplicationsController < ApplicationController
    end
 
    def create
-     application = Application.create(app_params)
-      if application.save
-        redirect_to "/applications/#{application.id}"
+     @application = Application.create(app_params)
+      if @application.save
+        redirect_to "/applications/#{@application.id}"
       else
         flash[:error] = "Error: Please Fill in ALL Fields"
         redirect_to '/applications/new'
       end
+    end
+
+    def update
+      @application = Application.find(params[:id])
+      @application.description = params[:fill_description]
+      @application.status = "Pending"
+      @application.save
+      redirect_to "/applications/#{@application.id}"
     end
 
    private
