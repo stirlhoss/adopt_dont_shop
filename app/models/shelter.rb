81727eaf8eed435b053fada64eb087b1e8a194de
Stirling Hostetter <stirhoss@icloud.com>
2022-05-22 14:34:16 -0500

    Add class method for show_pending

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -13,6 +13,10 @@ class Shelter < ApplicationRecord
     order(name: :desc)
   end
 
+  def self.show_pending
+    Shelter.joins(pets: :applications).where("applications.status = 'Pending'")
+  end
+
   def self.order_by_number_of_pets
     select("shelters.*, count(pets.id) AS pets_count")
       .joins("LEFT OUTER JOIN pets ON pets.shelter_id = shelters.id")
