efe0d62cd73d7f363291d3972542c28d0fe8804e
Stirling Hostetter <stirhoss@icloud.com>
2022-05-23 17:33:29 -0500

    Feat: add all approved condition to controller

--- app/controllers/admin/applications_controller.rb
+++ app/controllers/admin/applications_controller.rb
@@ -8,6 +8,10 @@ class Admin::ApplicationsController < ApplicationController
     pet_application = PetApplication.pet_app_find(@application.id, params[:pet_id])
     pet_application.status = params[:pet_status]
     pet_application.save
+    if @application.all_approved?
+      @application.status = "Approved"
+      @application.save
+    end
     redirect_to "/admin/applications/#{@application.id}"
   end
 end
