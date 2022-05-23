98be386d46aaac7776c8fb7c76cd6e8c93c8e4fb
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-23 15:10:51 -0500

    Feat: pet_app_find method created

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,9 @@
 class PetApplication < ApplicationRecord
    belongs_to :pet
    belongs_to :application
+
+   def self.pet_app_find(applicationid,petid)
+     PetApplication.where("application_id = ? and pet_id = ?", applicationid, petid).first
+   end
+
  end
