2ce9f1a1ed51259ab8aea02be589d19265619cf4
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-23 15:12:22 -0500

    Feat: Update method created

---
+++ app/controllers/admin/applications_controller.rb
@@ -0,0 +1,13 @@
+class Admin::ApplicationsController < ApplicationController
+  def show
+    @application = Application.find(params[:id])
+  end
+
+  def update
+    @application = Application.find(params[:id])
+    pet_application = PetApplication.pet_app_find(@application.id, params[:pet_id])
+    pet_application.status = params[:pet_status]
+    pet_application.save
+    redirect_to "/admin/applications/#{@application.id}"
+  end
+end
