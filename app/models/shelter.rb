83a3cc4501b9661ffd1e7dfbe5370a1a57392a28
Stirling Hostetter <stirhoss@icloud.com>
2022-05-24 13:06:00 -0500

    Fix: forgot ; in sql statement

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -10,7 +10,7 @@ class Shelter < ApplicationRecord
   end
 
   def self.order_by_name_desc
-    find_by_sql("select * from shelters order by shelters.name desc")
+    find_by_sql("SELECT * FROM shelters ORDER BY name desc;")
   end
 
   def self.show_pending
