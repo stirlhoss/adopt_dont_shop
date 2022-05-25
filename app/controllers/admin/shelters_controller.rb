41b8e1ec01c95f59c22780b4e5408d02b6214ded
Stirling Hostetter <stirhoss@icloud.com>
2022-05-25 13:46:22 -0500

    Feat: add method for show_pending_pets

--- app/controllers/admin/shelters_controller.rb
+++ app/controllers/admin/shelters_controller.rb
@@ -8,5 +8,6 @@ class Admin::SheltersController < ApplicationController
     @shelter = Shelter.only_name_and_city(params[:id])
     @shelter_all = Shelter.find(params[:id])
     @pets = @shelter_all.pets
+    @pending_pets = @shelter_all.show_pending_pets
   end
 end
