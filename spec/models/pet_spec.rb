bfb2273e76435b9bf6a2792d4de11fd13731f127
Nick Jacobs <nickjacobsatc@gmail.com>
2022-05-25 11:53:17 -0500

    Feat: avg_age number_of_pets number_of_adopted methods created

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -14,11 +14,30 @@ RSpec.describe Pet, type: :model do
   before(:each) do
     @shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
     @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 5, adoptable: true)
     @pet_3 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
   end
 
   describe 'class methods' do
+
+    describe '#number_of_adopted' do
+      it "shows number of pets that have been adopted" do
+        expect(Pet.number_of_adopted).to eq(1)
+      end
+    end
+
+    describe '#number_of_pets' do
+      it "counts the number of pets" do
+        expect(Pet.number_of_pets).to eq(2)
+      end
+    end
+
+    describe '#avg_age' do
+      it "gives average age of pets" do
+        expect(Pet.avg_age).to eq(5)
+      end
+    end
+
     describe '#search' do
       it 'returns partial matches' do
         expect(Pet.search("Claw")).to eq([@pet_2])
